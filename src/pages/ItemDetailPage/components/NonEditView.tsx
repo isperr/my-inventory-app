1621053f5b21e307d119869e1dab3fa2cf40ed81
isperr <isperr.mmt-b2016@fh-salzburg.ac.at>
2025-05-01 16:27:03 +0200

    fix(non-edit-view): fix displaying price

--- src/pages/ItemDetailPage/components/NonEditView.tsx
+++ src/pages/ItemDetailPage/components/NonEditView.tsx
@@ -1,4 +1,4 @@
-import {memo, useMemo} from 'react'
+import {memo} from 'react'
 import {twMerge} from 'tailwind-merge'
 import {Box, Typography} from '@mui/material'
 import ImageIcon from '@mui/icons-material/Image'
@@ -23,56 +23,53 @@ const NonEditView = ({
   count,
   details,
   category
-}: NonEditViewProps) => {
-  const formattedPrice = useMemo(() => {
-    const lang = navigator.languages?.[0] ?? 'de-DE'
-
-    return new Intl.NumberFormat(lang, {
-      style: 'currency',
-      currency: 'EUR'
-    }).format(price)
-  }, [price, navigator.languages?.[0]])
-  return (
-    <>
-      <Typography className="px-4 text-center" variant="h4">
-        {name}
-      </Typography>
-      {imgUrl ? (
-        <img
-          className="max-h-[180px] min-h-[150px] min-w-[250px] max-w-[300px] m-auto object-contain"
-          alt={name}
-          src={imgUrl}
-        />
-      ) : (
-        <Box
-          className={twMerge(
-            'h-[180px] w-[250px] mx-auto my-3.5 text-[4rem]',
-            'bg-[#bdbdbd] flex justify-center items-center text-[#f4f3f2]'
-          )}
-        >
-          <ImageIcon color="inherit" fontSize="inherit" />
-        </Box>
-      )}
-      <Box className="mx-6">
-        <DetailText heading="Daten" type="item" />
-        <div className="grid grid-cols-10">
-          <DetailText
-            heading="Kategorie:"
-            text={categoryNames[category]}
-            type="item"
-          />
-          <DetailText heading="Anzahl:" text={count.toString()} type="item" />
-          <DetailText heading="Preis:" text={formattedPrice} type="item" />
-          <DetailText heading="Details:" text={details || '--'} type="item" />
-        </div>
+}: NonEditViewProps) => (
+  <>
+    <Typography className="px-4 text-center" variant="h4">
+      {name}
+    </Typography>
+    {imgUrl ? (
+      <img
+        className="max-h-[180px] min-h-[150px] min-w-[250px] max-w-[300px] m-auto object-contain"
+        alt={name}
+        src={imgUrl}
+      />
+    ) : (
+      <Box
+        className={twMerge(
+          'h-[180px] w-[250px] mx-auto my-3.5 text-[4rem]',
+          'bg-[#bdbdbd] flex justify-center items-center text-[#f4f3f2]'
+        )}
+      >
+        <ImageIcon color="inherit" fontSize="inherit" />
       </Box>
+    )}
+    <Box className="mx-6">
+      <DetailText heading="Daten" type="item" />
+      <div className="grid grid-cols-10">
+        <DetailText
+          heading="Kategorie:"
+          text={categoryNames[category]}
+          type="item"
+        />
+        <DetailText heading="Anzahl:" text={count.toString()} type="item" />
+        <DetailText
+          heading="Preis:"
+          text={new Intl.NumberFormat(navigator.languages?.[0] ?? 'de-DE', {
+            style: 'currency',
+            currency: 'EUR'
+          }).format(price)}
+          type="item"
+        />
+        <DetailText heading="Details:" text={details || '--'} type="item" />
+      </div>
+    </Box>
 
-      <Button className="mx-6" onClick={enterEditMode} startIcon={<EditIcon />}>
-        Bearbeiten
-      </Button>
-      <DeleteDialog hasImage={Boolean(imgUrl)} id={id} name={name} />
-    </>
-  )
-}
+    <Button className="mx-6" onClick={enterEditMode} startIcon={<EditIcon />}>
+      Bearbeiten
+    </Button>
+    <DeleteDialog hasImage={Boolean(imgUrl)} id={id} name={name} />
+  </>
+)
 
 export default memo(NonEditView)
