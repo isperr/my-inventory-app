1a969edcdc59b7235f33d52dd89cbb18ccc860f2
isperr <isperr.mmt-b2016@fh-salzburg.ac.at>
2025-05-03 20:38:31 +0200

    fix(non-edit-view): allow preview image to be in slider

--- src/molecules/ItemImagesViewer/index.tsx
+++ src/molecules/ItemImagesViewer/index.tsx
@@ -1,45 +1,72 @@
 import {PhotoSlider} from 'react-photo-view'
 import ImageList from '@mui/material/ImageList'
 import ImageListItem from '@mui/material/ImageListItem'
-import {useState} from 'react'
+import {useMemo, useState} from 'react'
+import {ExtraImageType} from '../../modules/types'
 
-const ItemImagesViewer = ({imgUrls}: {imgUrls: Array<string>}) => {
+const ItemImagesViewer = ({
+  images,
+  isSliderOpen,
+  preview,
+  setIsSliderOpen
+}: {
+  images: Array<ExtraImageType>
+  isSliderOpen: boolean
+  preview?: string
+  setIsSliderOpen: (isSliderOpen: boolean) => void
+}) => {
   const [index, setIndex] = useState<number>(0)
-  const [visible, setVisible] = useState<boolean>(false)
+
+  const imgUrls = useMemo(() => {
+    const temp: Array<{showInList: boolean; imgUrl: string}> = images.map(
+      ({imgUrl}) => ({imgUrl, showInList: true})
+    )
+
+    if (preview) {
+      temp.unshift({showInList: false, imgUrl: preview})
+    }
+
+    return temp
+  }, [preview, images])
 
   return (
     <>
       <ImageList className="px-6" cols={3} rowHeight={120} variant="quilted">
-        {imgUrls.map((imgUrl, idx) => (
-          <ImageListItem
-            className="cursor-pointer"
-            key={`img-${idx}`}
-            onClick={() => {
-              setIndex(idx)
-              setVisible(true)
-            }}
-            onKeyDown={event => {
-              if (event.key === 'Enter') {
-                setIndex(idx)
-                setVisible(true)
-              }
-            }}
-            tabIndex={0}
-          >
-            <img
-              srcSet={imgUrl}
-              src={imgUrl}
-              alt={`img-${idx}`}
-              loading="lazy"
-            />
-          </ImageListItem>
-        ))}
+        {imgUrls.map(({imgUrl, showInList}, idx) => {
+          if (showInList) {
+            return (
+              <ImageListItem
+                className="cursor-pointer"
+                key={`img-${idx}`}
+                onClick={() => {
+                  setIndex(idx)
+                  setIsSliderOpen(true)
+                }}
+                onKeyDown={event => {
+                  if (event.key === 'Enter') {
+                    setIndex(idx)
+                    setIsSliderOpen(true)
+                  }
+                }}
+                tabIndex={0}
+              >
+                <img
+                  srcSet={imgUrl}
+                  src={imgUrl}
+                  alt={`img-${idx}`}
+                  loading="lazy"
+                />
+              </ImageListItem>
+            )
+          }
+          return null
+        })}
       </ImageList>
       <PhotoSlider
-        images={imgUrls.map(imgUrl => ({src: imgUrl, key: imgUrl}))}
-        visible={visible}
+        images={imgUrls.map(({imgUrl}) => ({src: imgUrl, key: imgUrl}))}
+        visible={isSliderOpen}
         onClose={() => {
-          setVisible(false)
+          setIsSliderOpen(false)
           setIndex(0)
         }}
         index={index}
--- src/pages/ItemDetailPage/components/NonEditView.tsx
+++ src/pages/ItemDetailPage/components/NonEditView.tsx
@@ -1,4 +1,4 @@
-import {memo, useMemo} from 'react'
+import {memo, useState} from 'react'
 import {twMerge} from 'tailwind-merge'
 import {Box, Typography} from '@mui/material'
 import ImageIcon from '@mui/icons-material/Image'
@@ -26,7 +26,7 @@ const NonEditView = ({
   details,
   category
 }: NonEditViewProps) => {
-  const imgUrls = useMemo(() => images.map(({imgUrl}) => imgUrl), [images])
+  const [isSliderOpen, setIsSliderOpen] = useState<boolean>(false)
 
   return (
     <>
@@ -35,9 +35,12 @@ const NonEditView = ({
       </Typography>
       {imgUrl ? (
         <img
-          className="max-h-[180px] min-h-[150px] min-w-[250px] max-w-[300px] m-auto object-contain"
+          className="max-h-[180px] min-h-[150px] min-w-[250px] max-w-[300px] m-auto object-contain cursor-pointer"
           alt={name}
           src={imgUrl}
+          onClick={() => {
+            setIsSliderOpen(true)
+          }}
         />
       ) : (
         <Box
@@ -79,7 +82,12 @@ const NonEditView = ({
         name={name}
       />
 
-      <ItemImagesViewer imgUrls={imgUrls} />
+      <ItemImagesViewer
+        images={images}
+        isSliderOpen={isSliderOpen}
+        preview={imgUrl}
+        setIsSliderOpen={setIsSliderOpen}
+      />
     </>
   )
 }
